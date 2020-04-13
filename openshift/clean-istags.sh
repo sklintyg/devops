ab3bed96c5dceef79fbd0a29da0379ed41316942
Daniel Petersson <daniel.petersson@soprasteria.com>
2020-04-13 19:29:32 +0200

    INTYGFV-12005: Cleanup spring5 builds as well

--- openshift/clean-istags.sh
+++ openshift/clean-istags.sh
@@ -12,8 +12,8 @@ KEEP=5
 IS_NAME=$1
 MAJOR_VERSION=$2
 
-if [ ${#MAJOR_VERSION} -lt 5 ]; then
-	echo "Error: major version must be at least charaacters, i.e X.Y.Z"
+if [ ${#MAJOR_VERSION} -lt 3 ]; then
+	echo "Error: major version must be at least charaacters, i.e X.Y"
 	exit 1
 fi
 
