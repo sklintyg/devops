bcaf5976cdc45b584ae42d78ae08817c23ee8de8
Erik Karlsson <erik.karlsson@soprasteria.com>
2022-02-17 08:50:34 +0100

    DEVENV: Added scripts that NMT use in test environments. (#37)
    * DEVENV: Added scripts that NMT use in test environments.
    
    * DEVENV: Added scripts that NMT use in test environments.

---
+++ scripts/bash/redisresetcache_PUcacheWC.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+CHECK=$(echo "info" |redis-cli -h ind-sint-redis1.ind1.sth.basefarm.net -p 6379  | grep master_host | awk -F':' '{print $2}'|strings)
+if [[ ! $CHECK ]]; then
+HOST="ind-sint-redis1.ind1.sth.basefarm.net"
+else
+HOST=`host $CHECK | awk '{print $5}' |sed 's/\.$//g'`;
+fi
+
+if [[ ! $HOST ]]; then echo "Something is wrong, exiting"; fi
+
+echo "clearing personCache:webcert:*"
+#echo "info replication"| redis-cli -h $HOST -p 6379
+echo " EVAL \"return redis.call('del', unpack(redis.call('keys', ARGV[1])))\" 0 personCache:webcert:*" | redis-cli -h $HOST -p 6379
