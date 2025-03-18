5674fa096c31d81cebb0950d08acc88365ceb06c
Lucas Lundstrom <35566573+LucasLundstrom@users.noreply.github.com>
2025-03-18 09:45:07 +0100

    K1J-851: add names to row count column (#72)

--- scripts/sql/count_certificate_by_id_all_services.sql
+++ scripts/sql/count_certificate_by_id_all_services.sql
@@ -21,20 +21,20 @@ BEGIN
 
 
     -- ******** COUNTING FROM INTYGSTJANST DATABASE ********
-   SELECT COUNT(*) AS row_count FROM intygstjanst.RELATION WHERE FROM_INTYG_ID = certIdToCount;
-   SELECT COUNT(*) AS row_count FROM intygstjanst.RELATION WHERE TO_INTYG_ID = certIdToCount;
-   SELECT COUNT(*) AS row_count FROM intygstjanst.ARENDE WHERE INTYGS_ID = certIdToCount;
-   SELECT COUNT(*) AS row_count FROM intygstjanst.APPROVED_RECEIVER WHERE CERTIFICATE_ID = certIdToCount;
-   SELECT COUNT(*) AS row_count FROM intygstjanst.SJUKFALL_CERT WHERE ID = certIdToCount;
-   SELECT COUNT(*) AS row_count FROM intygstjanst.SJUKFALL_CERT_WORK_CAPACITY WHERE CERTIFICATE_ID = certIdToCount;
-   SELECT COUNT(*) AS row_count FROM intygstjanst.CERTIFICATE_METADATA WHERE CERTIFICATE_ID = certIdToCount;
-   SELECT COUNT(*) AS row_count FROM intygstjanst.CERTIFICATE_STATE WHERE CERTIFICATE_ID = certIdToCount;
-   SELECT COUNT(*) AS row_count FROM intygstjanst.ORIGINAL_CERTIFICATE WHERE CERTIFICATE_ID = certIdToCount;
-   SELECT COUNT(*) AS row_count FROM intygstjanst.CERTIFICATE WHERE ID = certIdToCount;
+   SELECT COUNT(*) AS row_count_intygstjanst_RELATION FROM intygstjanst.RELATION WHERE FROM_INTYG_ID = certIdToCount;
+   SELECT COUNT(*) AS row_count_intygstjanst_RELATION FROM intygstjanst.RELATION WHERE TO_INTYG_ID = certIdToCount;
+   SELECT COUNT(*) AS row_count_intygstjanst_ARENDE FROM intygstjanst.ARENDE WHERE INTYGS_ID = certIdToCount;
+   SELECT COUNT(*) AS row_count_intygstjanst_APPROVED_RECEIVER FROM intygstjanst.APPROVED_RECEIVER WHERE CERTIFICATE_ID = certIdToCount;
+   SELECT COUNT(*) AS row_count_intygstjanst_SJUKFALL_CERT FROM intygstjanst.SJUKFALL_CERT WHERE ID = certIdToCount;
+   SELECT COUNT(*) AS row_count_intygstjanst_SJUKFALL_CERT_WORK_CAPACITY FROM intygstjanst.SJUKFALL_CERT_WORK_CAPACITY WHERE CERTIFICATE_ID = certIdToCount;
+   SELECT COUNT(*) AS row_count_intygstjanst_CERTIFICATE_METADATA FROM intygstjanst.CERTIFICATE_METADATA WHERE CERTIFICATE_ID = certIdToCount;
+   SELECT COUNT(*) AS row_count_intygstjanst_CERTIFICATE_STATE FROM intygstjanst.CERTIFICATE_STATE WHERE CERTIFICATE_ID = certIdToCount;
+   SELECT COUNT(*) AS row_count_intygstjanst_ORIGINAL_CERTIFICATE FROM intygstjanst.ORIGINAL_CERTIFICATE WHERE CERTIFICATE_ID = certIdToCount;
+   SELECT COUNT(*) AS row_count_intygstjanst_CERTIFICATE FROM intygstjanst.CERTIFICATE WHERE ID = certIdToCount;
 
 
     -- ******** COUNTING FROM INTYGSADMIN DATABASE ********
-   SELECT COUNT(*) AS row_count FROM intygsadmin.intyg_info WHERE intyg_id = certIdToCount;
+   SELECT COUNT(*) AS row_count_intygsadmin_intyg_info FROM intygsadmin.intyg_info WHERE intyg_id = certIdToCount;
 
 
     -- ******** COUNTING FROM STATISTIK DATABASE ********
@@ -44,13 +44,13 @@ BEGIN
                     PRIMARY KEY (S_M_KEY));
     INSERT INTO STATISTIK_MESSAGE_KEYS (S_M_KEY) SELECT meddelandeId FROM statistik.messagewideline WHERE intygid = certIdToCount;
 
-   SELECT COUNT(*) AS row_count FROM statistik.intyghandelse WHERE CORRELATIONID = certIdToCount;
-   SELECT COUNT(*) AS row_count FROM statistik.intygsenthandelse WHERE CORRELATIONID = certIdToCount;
-   SELECT COUNT(*) AS row_count FROM statistik.intygcommon WHERE INTYGID = certIdToCount;
-   SELECT COUNT(*) AS row_count FROM statistik.messagewideline WHERE intygid = certIdToCount;
-   SELECT COUNT(*) AS row_count FROM statistik.wideline WHERE correlationId = certIdToCount;
-   SELECT COUNT(*) AS row_count FROM statistik.meddelandehandelse WHERE correlationId IN (SELECT S_M_KEY FROM STATISTIK_MESSAGE_KEYS);
-   SELECT COUNT(*) AS row_count FROM statistik.hsa WHERE id = certIdToCount;
+   SELECT COUNT(*) AS row_count_statistik_intyghandelse FROM statistik.intyghandelse WHERE CORRELATIONID = certIdToCount;
+   SELECT COUNT(*) AS row_count_statistik_intygsenthandelse FROM statistik.intygsenthandelse WHERE CORRELATIONID = certIdToCount;
+   SELECT COUNT(*) AS row_count_statistik_intygcommon FROM statistik.intygcommon WHERE INTYGID = certIdToCount;
+   SELECT COUNT(*) AS row_count_statistik_messagewideline FROM statistik.messagewideline WHERE intygid = certIdToCount;
+   SELECT COUNT(*) AS row_count_statistik_wideline FROM statistik.wideline WHERE correlationId = certIdToCount;
+   SELECT COUNT(*) AS row_count_statistik_meddelandehandelse FROM statistik.meddelandehandelse WHERE correlationId IN (SELECT S_M_KEY FROM STATISTIK_MESSAGE_KEYS);
+   SELECT COUNT(*) AS row_count_statistik_hsa FROM statistik.hsa WHERE id = certIdToCount;
 
 
     -- ******** COUNTING FROM WEBCERT DATABASE ********
@@ -74,23 +74,23 @@ BEGIN
 
 
 
-    SELECT COUNT(*) AS row_count FROM webcert.CERTIFICATE_EVENT WHERE CERTIFICATE_ID = certIdToCount;
-    SELECT COUNT(*) AS row_count FROM webcert.NOTIFICATION_REDELIVERY WHERE HANDELSE_ID IN (SELECT H_ID FROM WEBCERT_HANDELSE_IDS);
-    SELECT COUNT(*) AS row_count FROM webcert.HANDELSE_METADATA WHERE HANDELSE_ID IN (SELECT H_ID FROM WEBCERT_HANDELSE_IDS);
-    SELECT COUNT(*) AS row_count FROM webcert.HANDELSE WHERE INTYGS_ID = certIdToCount;
-    SELECT COUNT(*) AS row_count FROM webcert.SIGNATUR WHERE INTYG_ID = certIdToCount;
-    SELECT COUNT(*) AS row_count FROM webcert.REFERENS WHERE INTYG_ID = certIdToCount;
-    SELECT COUNT(*) AS row_count FROM webcert.PAGAENDE_SIGNERING WHERE INTYG_ID = certIdToCount;
-    SELECT COUNT(*) AS row_count FROM webcert.ARENDE_KONTAKT_INFO WHERE ARENDE_ID IN (SELECT A_ID FROM WEBCERT_ARENDE_IDS);
-    SELECT COUNT(*) AS row_count FROM webcert.MEDICINSKT_ARENDE WHERE ARENDE_ID IN (SELECT A_ID FROM WEBCERT_ARENDE_IDS);
-    SELECT COUNT(*) AS row_count FROM webcert.ARENDE WHERE INTYGS_ID = certIdToCount;
-    SELECT COUNT(*) AS row_count FROM webcert.ARENDE_UTKAST WHERE INTYGS_ID = certIdToCount;
-    SELECT COUNT(*) AS row_count FROM webcert.CERTIFICATE_EVENT_FAILED_LOAD WHERE CERTIFICATE_ID = certIdToCount;
-    SELECT COUNT(*) AS row_count FROM webcert.CERTIFICATE_EVENT_PROCESSED WHERE CERTIFICATE_ID = certIdToCount;
-    SELECT COUNT(*) AS row_count FROM webcert.KOMPLETTERING WHERE FRAGASVAR_ID IN (SELECT F_REF FROM WEBCERT_FRAGASVAR_REFS);
-    SELECT COUNT(*) AS row_count FROM webcert.EXTERNA_KONTAKTER WHERE FRAGASVAR_ID IN (SELECT F_REF FROM WEBCERT_FRAGASVAR_REFS);
-    SELECT COUNT(*) AS row_count FROM webcert.FRAGASVAR WHERE INTYGS_ID = certIdToCount;
-    SELECT COUNT(*) AS row_count FROM webcert.INTYG WHERE INTYGS_ID = certIdToCount;
+    SELECT COUNT(*) AS row_count_webcert_CERTIFICATE_EVENT FROM webcert.CERTIFICATE_EVENT WHERE CERTIFICATE_ID = certIdToCount;
+    SELECT COUNT(*) AS row_count_webcert_NOTIFICATION_REDELIVERY FROM webcert.NOTIFICATION_REDELIVERY WHERE HANDELSE_ID IN (SELECT H_ID FROM WEBCERT_HANDELSE_IDS);
+    SELECT COUNT(*) AS row_count_webcert_HANDELSE_METADATA FROM webcert.HANDELSE_METADATA WHERE HANDELSE_ID IN (SELECT H_ID FROM WEBCERT_HANDELSE_IDS);
+    SELECT COUNT(*) AS row_count_webcert_HANDELSE FROM webcert.HANDELSE WHERE INTYGS_ID = certIdToCount;
+    SELECT COUNT(*) AS row_count_webcert_SIGNATUR FROM webcert.SIGNATUR WHERE INTYG_ID = certIdToCount;
+    SELECT COUNT(*) AS row_count_webcert_REFERENS FROM webcert.REFERENS WHERE INTYG_ID = certIdToCount;
+    SELECT COUNT(*) AS row_count_webcert_PAGAENDE_SIGNERING FROM webcert.PAGAENDE_SIGNERING WHERE INTYG_ID = certIdToCount;
+    SELECT COUNT(*) AS row_count_webcert_ARENDE_KONTAKT_INFO FROM webcert.ARENDE_KONTAKT_INFO WHERE ARENDE_ID IN (SELECT A_ID FROM WEBCERT_ARENDE_IDS);
+    SELECT COUNT(*) AS row_count_webcert_MEDICINSKT_ARENDE FROM webcert.MEDICINSKT_ARENDE WHERE ARENDE_ID IN (SELECT A_ID FROM WEBCERT_ARENDE_IDS);
+    SELECT COUNT(*) AS row_count_webcert_ARENDE FROM webcert.ARENDE WHERE INTYGS_ID = certIdToCount;
+    SELECT COUNT(*) AS row_count_webcert_ARENDE_UTKAST FROM webcert.ARENDE_UTKAST WHERE INTYGS_ID = certIdToCount;
+    SELECT COUNT(*) AS row_count_webcert_CERTIFICATE_EVENT_FAILED_LOAD FROM webcert.CERTIFICATE_EVENT_FAILED_LOAD WHERE CERTIFICATE_ID = certIdToCount;
+    SELECT COUNT(*) AS row_count_webcert_CERTIFICATE_EVENT_PROCESSED FROM webcert.CERTIFICATE_EVENT_PROCESSED WHERE CERTIFICATE_ID = certIdToCount;
+    SELECT COUNT(*) AS row_count_webcert_KOMPLETTERING FROM webcert.KOMPLETTERING WHERE FRAGASVAR_ID IN (SELECT F_REF FROM WEBCERT_FRAGASVAR_REFS);
+    SELECT COUNT(*) AS row_count_webcert_EXTERNA_KONTAKTER FROM webcert.EXTERNA_KONTAKTER WHERE FRAGASVAR_ID IN (SELECT F_REF FROM WEBCERT_FRAGASVAR_REFS);
+    SELECT COUNT(*) AS row_count_webcert_FRAGASVAR FROM webcert.FRAGASVAR WHERE INTYGS_ID = certIdToCount;
+    SELECT COUNT(*) AS row_count_webcert_INTYG FROM webcert.INTYG WHERE INTYGS_ID = certIdToCount;
 
 
 
@@ -106,18 +106,18 @@ BEGIN
     -- Insert message keys into the temporary table
     INSERT INTO CS_MESSAGE_KEYS (CS_M_KEY) SELECT `KEY` FROM certificate_service.message WHERE certificate_key = csCertKey;
 
-   SELECT COUNT(*) AS row_count FROM certificate_service.message_complement WHERE MESSAGE_KEY IN (SELECT CS_M_KEY FROM CS_MESSAGE_KEYS);
-   SELECT COUNT(*) AS row_count FROM certificate_service.message_contact_info WHERE MESSAGE_KEY IN (SELECT CS_M_KEY FROM CS_MESSAGE_KEYS);
-   SELECT COUNT(*) AS row_count FROM certificate_service.message_relation WHERE PARENT_MESSAGE_KEY IN (SELECT CS_M_KEY FROM CS_MESSAGE_KEYS);
-   SELECT COUNT(*) AS row_count FROM certificate_service.message_relation WHERE CHILD_MESSAGE_KEY IN (SELECT CS_M_KEY FROM CS_MESSAGE_KEYS);
-   SELECT COUNT(*) AS row_count FROM certificate_service.message WHERE CERTIFICATE_KEY = csCertKey;
+   SELECT COUNT(*) AS row_count_certificate_service_message_complement FROM certificate_service.message_complement WHERE MESSAGE_KEY IN (SELECT CS_M_KEY FROM CS_MESSAGE_KEYS);
+   SELECT COUNT(*) AS row_count_certificate_service_message_contact_info FROM certificate_service.message_contact_info WHERE MESSAGE_KEY IN (SELECT CS_M_KEY FROM CS_MESSAGE_KEYS);
+   SELECT COUNT(*) AS row_count_certificate_service_message_relation FROM certificate_service.message_relation WHERE PARENT_MESSAGE_KEY IN (SELECT CS_M_KEY FROM CS_MESSAGE_KEYS);
+   SELECT COUNT(*) AS row_count_certificate_service_message_relation FROM certificate_service.message_relation WHERE CHILD_MESSAGE_KEY IN (SELECT CS_M_KEY FROM CS_MESSAGE_KEYS);
+   SELECT COUNT(*) AS row_count_certificate_service_message FROM certificate_service.message WHERE CERTIFICATE_KEY = csCertKey;
 
-   SELECT COUNT(*) AS row_count FROM certificate_service.certificate_xml WHERE `KEY` = csCertKey;
-   SELECT COUNT(*) AS row_count FROM certificate_service.certificate_relation WHERE PARENT_CERTIFICATE_KEY = csCertKey;
-   SELECT COUNT(*) AS row_count FROM certificate_service.certificate_relation WHERE CHILD_CERTIFICATE_KEY = csCertKey;
-   SELECT COUNT(*) AS row_count FROM certificate_service.certificate_data WHERE `KEY` = csCertKey;
-   SELECT COUNT(*) AS row_count FROM certificate_service.external_reference WHERE `KEY` = csCertKey;
-   SELECT COUNT(*) AS row_count FROM certificate_service.certificate WHERE CERTIFICATE_ID = certIdToCountCS;
+   SELECT COUNT(*) AS row_count_certificate_service_certificate_xml FROM certificate_service.certificate_xml WHERE `KEY` = csCertKey;
+   SELECT COUNT(*) AS row_count_certificate_service_certificate_relation FROM certificate_service.certificate_relation WHERE PARENT_CERTIFICATE_KEY = csCertKey;
+   SELECT COUNT(*) AS row_count_certificate_service_certificate_relation FROM certificate_service.certificate_relation WHERE CHILD_CERTIFICATE_KEY = csCertKey;
+   SELECT COUNT(*) AS row_count_certificate_service_certificate_data FROM certificate_service.certificate_data WHERE `KEY` = csCertKey;
+   SELECT COUNT(*) AS row_count_certificate_service_external_reference FROM certificate_service.external_reference WHERE `KEY` = csCertKey;
+   SELECT COUNT(*) AS row_count_certificate_service_certificate FROM certificate_service.certificate WHERE CERTIFICATE_ID = certIdToCountCS;
 
     DROP TEMPORARY TABLE CS_MESSAGE_KEYS;
     DROP TEMPORARY TABLE STATISTIK_MESSAGE_KEYS;
