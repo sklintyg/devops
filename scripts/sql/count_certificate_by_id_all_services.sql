c1cd3e8eeac69edc71b0c6f5b3f5899abba7eb83
Lucas Lundstrom <35566573+LucasLundstrom@users.noreply.github.com>
2025-03-18 08:12:21 +0100

    K1J-851: add COLLATE to declared var (#71)

--- scripts/sql/count_certificate_by_id_all_services.sql
+++ scripts/sql/count_certificate_by_id_all_services.sql
@@ -1,11 +1,12 @@
 USE intygstjanst;
 DELIMITER $$
-CREATE PROCEDURE select_certificate_by_id_all_services()
+CREATE PROCEDURE count_certificate_by_id_all_services()
 BEGIN
     DECLARE errorCode CHAR(5) DEFAULT '00000';
     DECLARE errorMessage TEXT;
     DECLARE csCertKey BIGINT;
-    DECLARE certIdToDelete VARCHAR(255) DEFAULT '<certificate_id>';
+    DECLARE certIdToCount VARCHAR(255) CHARACTER SET utf8mb3 COLLATE utf8mb3_general_ci DEFAULT '<certificate_id>';
+    DECLARE certIdToCountCS VARCHAR(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci DEFAULT '<certificate_id>';
 
     DECLARE CONTINUE HANDLER FOR SQLEXCEPTION
     BEGIN
@@ -19,87 +20,87 @@ BEGIN
     SET FOREIGN_KEY_CHECKS = 0;
 
 
-    -- ******** DELETING FROM INTYGSTJANST DATABASE ********
-   SELECT COUNT(*) AS row_count FROM intygstjanst.RELATION WHERE FROM_INTYG_ID = certIdToDelete;
-   SELECT COUNT(*) AS row_count FROM intygstjanst.RELATION WHERE TO_INTYG_ID = certIdToDelete;
-   SELECT COUNT(*) AS row_count FROM intygstjanst.ARENDE WHERE INTYGS_ID = certIdToDelete;
-   SELECT COUNT(*) AS row_count FROM intygstjanst.APPROVED_RECEIVER WHERE CERTIFICATE_ID = certIdToDelete;
-   SELECT COUNT(*) AS row_count FROM intygstjanst.SJUKFALL_CERT WHERE ID = certIdToDelete;
-   SELECT COUNT(*) AS row_count FROM intygstjanst.SJUKFALL_CERT_WORK_CAPACITY WHERE CERTIFICATE_ID = certIdToDelete;
-   SELECT COUNT(*) AS row_count FROM intygstjanst.CERTIFICATE_METADATA WHERE CERTIFICATE_ID = certIdToDelete;
-   SELECT COUNT(*) AS row_count FROM intygstjanst.CERTIFICATE_STATE WHERE CERTIFICATE_ID = certIdToDelete;
-   SELECT COUNT(*) AS row_count FROM intygstjanst.ORIGINAL_CERTIFICATE WHERE CERTIFICATE_ID = certIdToDelete;
-   SELECT COUNT(*) AS row_count FROM intygstjanst.CERTIFICATE WHERE ID = certIdToDelete;
+    -- ******** COUNTING FROM INTYGSTJANST DATABASE ********
+   SELECT COUNT(*) AS row_count FROM intygstjanst.RELATION WHERE FROM_INTYG_ID = certIdToCount;
+   SELECT COUNT(*) AS row_count FROM intygstjanst.RELATION WHERE TO_INTYG_ID = certIdToCount;
+   SELECT COUNT(*) AS row_count FROM intygstjanst.ARENDE WHERE INTYGS_ID = certIdToCount;
+   SELECT COUNT(*) AS row_count FROM intygstjanst.APPROVED_RECEIVER WHERE CERTIFICATE_ID = certIdToCount;
+   SELECT COUNT(*) AS row_count FROM intygstjanst.SJUKFALL_CERT WHERE ID = certIdToCount;
+   SELECT COUNT(*) AS row_count FROM intygstjanst.SJUKFALL_CERT_WORK_CAPACITY WHERE CERTIFICATE_ID = certIdToCount;
+   SELECT COUNT(*) AS row_count FROM intygstjanst.CERTIFICATE_METADATA WHERE CERTIFICATE_ID = certIdToCount;
+   SELECT COUNT(*) AS row_count FROM intygstjanst.CERTIFICATE_STATE WHERE CERTIFICATE_ID = certIdToCount;
+   SELECT COUNT(*) AS row_count FROM intygstjanst.ORIGINAL_CERTIFICATE WHERE CERTIFICATE_ID = certIdToCount;
+   SELECT COUNT(*) AS row_count FROM intygstjanst.CERTIFICATE WHERE ID = certIdToCount;
 
 
-    -- ******** DELETING FROM INTYGSADMIN DATABASE ********
-   SELECT COUNT(*) AS row_count FROM intygsadmin.intyg_info WHERE intyg_id = certIdToDelete;
+    -- ******** COUNTING FROM INTYGSADMIN DATABASE ********
+   SELECT COUNT(*) AS row_count FROM intygsadmin.intyg_info WHERE intyg_id = certIdToCount;
 
 
-    -- ******** DELETING FROM STATISTIK DATABASE ********
+    -- ******** COUNTING FROM STATISTIK DATABASE ********
     -- Create temporary table to store statistik.meddelandehandelse correlationId
     CREATE TEMPORARY TABLE STATISTIK_MESSAGE_KEYS (
                     S_M_KEY BIGINT NOT NULL COLLATE utf8mb3_general_ci,
                     PRIMARY KEY (S_M_KEY));
-    INSERT INTO STATISTIK_MESSAGE_KEYS (S_M_KEY) SELECT meddelandeId FROM statistik.messagewideline WHERE intygid = certIdToDelete;
+    INSERT INTO STATISTIK_MESSAGE_KEYS (S_M_KEY) SELECT meddelandeId FROM statistik.messagewideline WHERE intygid = certIdToCount;
 
-   SELECT COUNT(*) AS row_count FROM statistik.intyghandelse WHERE CORRELATIONID = certIdToDelete;
-   SELECT COUNT(*) AS row_count FROM statistik.intygsenthandelse WHERE CORRELATIONID = certIdToDelete;
-   SELECT COUNT(*) AS row_count FROM statistik.intygcommon WHERE INTYGID = certIdToDelete;
-   SELECT COUNT(*) AS row_count FROM statistik.messagewideline WHERE intygid = certIdToDelete;
-   SELECT COUNT(*) AS row_count FROM statistik.wideline WHERE correlationId = certIdToDelete;
+   SELECT COUNT(*) AS row_count FROM statistik.intyghandelse WHERE CORRELATIONID = certIdToCount;
+   SELECT COUNT(*) AS row_count FROM statistik.intygsenthandelse WHERE CORRELATIONID = certIdToCount;
+   SELECT COUNT(*) AS row_count FROM statistik.intygcommon WHERE INTYGID = certIdToCount;
+   SELECT COUNT(*) AS row_count FROM statistik.messagewideline WHERE intygid = certIdToCount;
+   SELECT COUNT(*) AS row_count FROM statistik.wideline WHERE correlationId = certIdToCount;
    SELECT COUNT(*) AS row_count FROM statistik.meddelandehandelse WHERE correlationId IN (SELECT S_M_KEY FROM STATISTIK_MESSAGE_KEYS);
-   SELECT COUNT(*) AS row_count FROM statistik.hsa WHERE id = certIdToDelete;
+   SELECT COUNT(*) AS row_count FROM statistik.hsa WHERE id = certIdToCount;
 
 
-    -- ******** DELETING FROM WEBCERT DATABASE ********
+    -- ******** COUNTING FROM WEBCERT DATABASE ********
     -- Create temporary table to store webcert.HANDELSE INTYGS_ID
     CREATE TEMPORARY TABLE WEBCERT_HANDELSE_IDS (
                         H_ID BIGINT NOT NULL COLLATE utf8mb3_general_ci,
                         PRIMARY KEY (H_ID));
-    INSERT INTO WEBCERT_HANDELSE_IDS (H_ID) SELECT ID FROM webcert.HANDELSE WHERE INTYGS_ID = certIdToDelete;
+    INSERT INTO WEBCERT_HANDELSE_IDS (H_ID) SELECT ID FROM webcert.HANDELSE WHERE INTYGS_ID = certIdToCount;
 
     -- Create temporary table to store webcert.ARENDE ARENDE_ID
     CREATE TEMPORARY TABLE WEBCERT_ARENDE_IDS (
                     A_ID BIGINT NOT NULL COLLATE utf8mb3_general_ci,
                     PRIMARY KEY (A_ID));
-    INSERT INTO WEBCERT_ARENDE_IDS (A_ID) SELECT ID FROM webcert.ARENDE WHERE INTYGS_ID = certIdToDelete;
+    INSERT INTO WEBCERT_ARENDE_IDS (A_ID) SELECT ID FROM webcert.ARENDE WHERE INTYGS_ID = certIdToCount;
 
     -- Create temporary table to store webcert.FRAGASVAR internReferens
     CREATE TEMPORARY TABLE WEBCERT_FRAGASVAR_REFS (
                     F_REF BIGINT NOT NULL COLLATE utf8mb3_general_ci,
                     PRIMARY KEY (F_REF));
-    INSERT INTO WEBCERT_FRAGASVAR_REFS (F_REF) SELECT internReferens FROM webcert.FRAGASVAR WHERE INTYGS_ID = certIdToDelete;
+    INSERT INTO WEBCERT_FRAGASVAR_REFS (F_REF) SELECT internReferens FROM webcert.FRAGASVAR WHERE INTYGS_ID = certIdToCount;
 
 
 
-    SELECT COUNT(*) AS row_count FROM webcert.CERTIFICATE_EVENT WHERE CERTIFICATE_ID = certIdToDelete;
+    SELECT COUNT(*) AS row_count FROM webcert.CERTIFICATE_EVENT WHERE CERTIFICATE_ID = certIdToCount;
     SELECT COUNT(*) AS row_count FROM webcert.NOTIFICATION_REDELIVERY WHERE HANDELSE_ID IN (SELECT H_ID FROM WEBCERT_HANDELSE_IDS);
     SELECT COUNT(*) AS row_count FROM webcert.HANDELSE_METADATA WHERE HANDELSE_ID IN (SELECT H_ID FROM WEBCERT_HANDELSE_IDS);
-    SELECT COUNT(*) AS row_count FROM webcert.HANDELSE WHERE INTYGS_ID = certIdToDelete;
-    SELECT COUNT(*) AS row_count FROM webcert.SIGNATUR WHERE INTYG_ID = certIdToDelete;
-    SELECT COUNT(*) AS row_count FROM webcert.REFERENS WHERE INTYG_ID = certIdToDelete;
-    SELECT COUNT(*) AS row_count FROM webcert.PAGAENDE_SIGNERING WHERE INTYG_ID = certIdToDelete;
+    SELECT COUNT(*) AS row_count FROM webcert.HANDELSE WHERE INTYGS_ID = certIdToCount;
+    SELECT COUNT(*) AS row_count FROM webcert.SIGNATUR WHERE INTYG_ID = certIdToCount;
+    SELECT COUNT(*) AS row_count FROM webcert.REFERENS WHERE INTYG_ID = certIdToCount;
+    SELECT COUNT(*) AS row_count FROM webcert.PAGAENDE_SIGNERING WHERE INTYG_ID = certIdToCount;
     SELECT COUNT(*) AS row_count FROM webcert.ARENDE_KONTAKT_INFO WHERE ARENDE_ID IN (SELECT A_ID FROM WEBCERT_ARENDE_IDS);
     SELECT COUNT(*) AS row_count FROM webcert.MEDICINSKT_ARENDE WHERE ARENDE_ID IN (SELECT A_ID FROM WEBCERT_ARENDE_IDS);
-    SELECT COUNT(*) AS row_count FROM webcert.ARENDE WHERE INTYGS_ID = certIdToDelete;
-    SELECT COUNT(*) AS row_count FROM webcert.ARENDE_UTKAST WHERE INTYGS_ID = certIdToDelete;
-    SELECT COUNT(*) AS row_count FROM webcert.CERTIFICATE_EVENT_FAILED_LOAD WHERE CERTIFICATE_ID = certIdToDelete;
-    SELECT COUNT(*) AS row_count FROM webcert.CERTIFICATE_EVENT_PROCESSED WHERE CERTIFICATE_ID = certIdToDelete;
+    SELECT COUNT(*) AS row_count FROM webcert.ARENDE WHERE INTYGS_ID = certIdToCount;
+    SELECT COUNT(*) AS row_count FROM webcert.ARENDE_UTKAST WHERE INTYGS_ID = certIdToCount;
+    SELECT COUNT(*) AS row_count FROM webcert.CERTIFICATE_EVENT_FAILED_LOAD WHERE CERTIFICATE_ID = certIdToCount;
+    SELECT COUNT(*) AS row_count FROM webcert.CERTIFICATE_EVENT_PROCESSED WHERE CERTIFICATE_ID = certIdToCount;
     SELECT COUNT(*) AS row_count FROM webcert.KOMPLETTERING WHERE FRAGASVAR_ID IN (SELECT F_REF FROM WEBCERT_FRAGASVAR_REFS);
     SELECT COUNT(*) AS row_count FROM webcert.EXTERNA_KONTAKTER WHERE FRAGASVAR_ID IN (SELECT F_REF FROM WEBCERT_FRAGASVAR_REFS);
-    SELECT COUNT(*) AS row_count FROM webcert.FRAGASVAR WHERE INTYGS_ID = certIdToDelete;
-    SELECT COUNT(*) AS row_count FROM webcert.INTYG WHERE INTYGS_ID = certIdToDelete;
+    SELECT COUNT(*) AS row_count FROM webcert.FRAGASVAR WHERE INTYGS_ID = certIdToCount;
+    SELECT COUNT(*) AS row_count FROM webcert.INTYG WHERE INTYGS_ID = certIdToCount;
 
 
 
-    -- ******** DELETING FROM CERTIFICATE-SERVICE DATABASE ********
+    -- ******** COUNTING FROM CERTIFICATE-SERVICE DATABASE ********
     -- Insert cs certificate key into variable
-    SELECT `KEY` INTO csCertKey FROM certificate_service.certificate WHERE CERTIFICATE_ID = certIdToDelete;
+    SELECT `KEY` INTO csCertKey FROM certificate_service.certificate WHERE CERTIFICATE_ID = certIdToCountCS;
 
     -- Create temporary table to store Certificate-service message keys
     CREATE TEMPORARY TABLE CS_MESSAGE_KEYS (
-                    CS_M_KEY BIGINT NOT NULL COLLATE utf8mb3_general_ci,
+                    CS_M_KEY BIGINT NOT NULL COLLATE utf8mb4_0900_ai_ci,
                     PRIMARY KEY (CS_M_KEY)
                 );
     -- Insert message keys into the temporary table
@@ -116,7 +117,7 @@ BEGIN
    SELECT COUNT(*) AS row_count FROM certificate_service.certificate_relation WHERE CHILD_CERTIFICATE_KEY = csCertKey;
    SELECT COUNT(*) AS row_count FROM certificate_service.certificate_data WHERE `KEY` = csCertKey;
    SELECT COUNT(*) AS row_count FROM certificate_service.external_reference WHERE `KEY` = csCertKey;
-   SELECT COUNT(*) AS row_count FROM certificate_service.certificate WHERE CERTIFICATE_ID = certIdToDelete;
+   SELECT COUNT(*) AS row_count FROM certificate_service.certificate WHERE CERTIFICATE_ID = certIdToCountCS;
 
     DROP TEMPORARY TABLE CS_MESSAGE_KEYS;
     DROP TEMPORARY TABLE STATISTIK_MESSAGE_KEYS;
@@ -129,12 +130,12 @@ BEGIN
 
     IF errorCode = '00000' THEN
         COMMIT;
-    SELECT CONCAT('Successfully selected certificate with id = ', certIdToDelete, ' from all databases.') AS result_message;
+    SELECT CONCAT('Successfully counted certificate with id = ', certIdToCount, ' from all databases.') AS result_message;
     ELSE
         ROLLBACK;
         SELECT CONCAT('Stored procedure failed, no changes were introduced, errorCode = ', errorCode, ', errorMessage = ', errorMessage) AS result_message;
     END IF;
 END $$
 DELIMITER ;
-CALL select_certificate_by_id_all_services;
-DROP PROCEDURE select_certificate_by_id_all_services;
+CALL count_certificate_by_id_all_services;
+DROP PROCEDURE count_certificate_by_id_all_services;
