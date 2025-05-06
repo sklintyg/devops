3f45db86ebf28a4cedc761396b553f8802882029
mhornfeldt <michaela.hornfeldt@gmail.com>
2025-05-06 14:57:54 +0200

    K1J-975: Added variables and transaction to it script

--- scripts/sql/intygstjanst_update_care_provider.sql
+++ scripts/sql/intygstjanst_update_care_provider.sql
@@ -1,16 +1,62 @@
-USE intygstjanst;
+USE intyg;
 
--- Update CERTIFICATE table
-UPDATE CERTIFICATE
-SET CARE_GIVER_ID = 'SE2321000016-39KJ'
-WHERE CARE_GIVER_ID IN ('SE2321000016-1K2W', 'SE2321000016-11LS', 'SE2321000016-12B8', 'SE2321000016-3CLH');
+DELIMITER $$
+CREATE PROCEDURE updateCareProvider()
 
--- Update REKO table
-UPDATE REKO
-SET CARE_PROVIDER_ID = 'SE2321000016-39KJ'
-WHERE CARE_PROVIDER_ID IN ('SE2321000016-1K2W', 'SE2321000016-11LS', 'SE2321000016-12B8', 'SE2321000016-3CLH');
+BEGIN
+    -- Declare variables
+    DECLARE updatedCareProviderId VARCHAR(50);
+    DECLARE errorCode CHAR(5) DEFAULT '00000';
+    DECLARE errorMessage TEXT;
 
--- Update SJUKFALL_CERT table
-UPDATE SJUKFALL_CERT
-SET CARE_GIVER_ID = 'SE2321000016-39KJ'
-WHERE CARE_GIVER_ID IN ('SE2321000016-1K2W', 'SE2321000016-11LS', 'SE2321000016-12B8', 'SE2321000016-3CLH');
+    -- Declare handler
+    DECLARE CONTINUE HANDLER FOR SQLEXCEPTION
+    BEGIN
+        GET DIAGNOSTICS CONDITION 1
+            errorCode = RETURNED_SQLSTATE, errorMessage = MESSAGE_TEXT;
+    END;
+
+    SET updatedCareProviderId = 'SE2321000016-39KJ';
+
+    -- Start transaction
+    START TRANSACTION;
+
+    DROP TEMPORARY TABLE IF EXISTS originalCareProviderIds;
+    CREATE TEMPORARY TABLE originalCareProviderIds(id VARCHAR(50));
+
+    -- Insert original care provider IDs into the table variable
+    INSERT INTO originalCareProviderIds
+    VALUES ('SE2321000016-1K2W'), ('SE2321000016-11LS'), ('SE2321000016-12B8'), ('SE2321000016-3CLH');
+
+    -- Update CERTIFICATE table
+    UPDATE CERTIFICATE
+    SET CARE_GIVER_ID = updatedCareProviderId
+    WHERE CARE_GIVER_ID IN (SELECT Id FROM originalCareProviderIds);
+
+    -- Update REKO table
+    UPDATE REKO
+    SET CARE_PROVIDER_ID = updatedCareProviderId
+    WHERE CARE_PROVIDER_ID IN (SELECT Id FROM originalCareProviderIds);
+
+    -- Update SJUKFALL_CERT table
+    UPDATE SJUKFALL_CERT
+    SET CARE_GIVER_ID = updatedCareProviderId
+    WHERE CARE_GIVER_ID IN (SELECT Id FROM originalCareProviderIds);
+
+    DROP TEMPORARY TABLE IF EXISTS originalCareProviderIds;
+
+    IF errorCode = '00000' THEN
+        COMMIT;
+        SELECT 'Updated care provider successfully.';
+    ELSE
+        ROLLBACK;
+        SELECT 'Transaction rolled back due to sql exception. No changes were introduced.';
+        SELECT CONCAT('Stored procedure failed, error = ', errorCode, ', message = ', errorMessage);
+    END IF;
+
+END$$
+DELIMITER ;
+
+-- Call the stored procedure
+CALL updateCareProvider;
+DROP PROCEDURE updateCareProvider;
