aa542e150feee71ea8c58c769bc0a2e714567a99
mhornfeldt <michaela.hornfeldt@gmail.com>
2025-05-06 14:48:50 +0200

    K1J-975: Added variables and transaction to wc script

--- scripts/sql/webcert_update_care_provider.sql
+++ scripts/sql/webcert_update_care_provider.sql
@@ -1,21 +1,69 @@
 USE webcert;
 
--- Update FRAGASVAR table
-UPDATE FRAGASVAR
-SET VARDGIVAR_ID = 'SE2321000016-39KJ', VARDGIVARNAMN = 'Region Stockholm'
-WHERE VARDGIVAR_ID IN ('SE2321000016-1K2W', 'SE2321000016-11LS', 'SE2321000016-12B8', 'SE2321000016-3CLH');
+DELIMITER $$
+CREATE PROCEDURE updateCareProvider()
 
--- Update HANDELSE table
-UPDATE HANDELSE
-SET VARDGIVAR_ID = 'SE2321000016-39KJ'
-WHERE VARDGIVAR_ID IN ('SE2321000016-1K2W', 'SE2321000016-11LS', 'SE2321000016-12B8', 'SE2321000016-3CLH');
+BEGIN
+    -- Declare variables
+    DECLARE updatedCareProviderId VARCHAR(50);
+    DECLARE updatedCareProviderName VARCHAR(100);
+    DECLARE errorCode CHAR(5) DEFAULT '00000';
+    DECLARE errorMessage TEXT;
 
--- Update INTEGRERADE_VARDENHETER table
-UPDATE INTEGRERADE_VARDENHETER
-SET VARDGIVAR_ID = 'SE2321000016-39KJ', VARDGIVAR_NAMN = 'Region Stockholm'
-WHERE VARDGIVAR_ID IN ('SE2321000016-1K2W', 'SE2321000016-11LS', 'SE2321000016-12B8', 'SE2321000016-3CLH');
+    -- Declare handler
+    DECLARE CONTINUE HANDLER FOR SQLEXCEPTION
+    BEGIN
+        GET DIAGNOSTICS CONDITION 1
+            errorCode = RETURNED_SQLSTATE, errorMessage = MESSAGE_TEXT;
+    END;
 
--- Update INTYG table
-UPDATE INTYG
-SET VARDGIVAR_ID = 'SE2321000016-39KJ', VARDGIVAR_NAMN = 'Region Stockholm'
-WHERE VARDGIVAR_ID IN ('SE2321000016-1K2W', 'SE2321000016-11LS', 'SE2321000016-12B8', 'SE2321000016-3CLH');
+    SET updatedCareProviderId = 'SE2321000016-39KJ';
+    SET updatedCareProviderName = 'Region Stockholm';
+
+    -- Start transaction
+    START TRANSACTION;
+
+    DROP TEMPORARY TABLE IF EXISTS originalCareProviderIds;
+    CREATE TEMPORARY TABLE originalCareProviderIds(id VARCHAR(50));
+
+    -- Insert original care provider IDs into the table variable
+    INSERT INTO originalCareProviderIds
+    VALUES ('SE2321000016-1K2W'), ('SE2321000016-11LS'), ('SE2321000016-12B8'), ('SE2321000016-3CLH');
+
+    -- Update FRAGASVAR table
+    UPDATE FRAGASVAR
+    SET VARDGIVAR_ID = updatedCareProviderId, VARDGIVARNAMN = updatedCareProviderName
+    WHERE VARDGIVAR_ID IN (SELECT Id FROM originalCareProviderIds);
+
+    -- Update HANDELSE table
+    UPDATE HANDELSE
+    SET VARDGIVAR_ID = updatedCareProviderId
+    WHERE VARDGIVAR_ID IN (SELECT Id FROM originalCareProviderIds);
+
+    -- Update INTEGRERADE_VARDENHETER table
+    UPDATE INTEGRERADE_VARDENHETER
+    SET VARDGIVAR_ID = updatedCareProviderId, VARDGIVAR_NAMN = updatedCareProviderName
+    WHERE VARDGIVAR_ID IN (SELECT Id FROM originalCareProviderIds);
+
+    -- Update INTYG table
+    UPDATE INTYG
+    SET VARDGIVAR_ID = updatedCareProviderId, VARDGIVAR_NAMN = updatedCareProviderName
+    WHERE VARDGIVAR_ID IN (SELECT Id FROM originalCareProviderIds);
+
+    DROP TEMPORARY TABLE IF EXISTS originalCareProviderIds;
+
+    IF errorCode = '00000' THEN
+        COMMIT;
+        SELECT 'Updated care provider successfully.';
+    ELSE
+        ROLLBACK;
+        SELECT 'Transaction rolled back due to sql exception. No changes were introduced.';
+        SELECT CONCAT('Stored procedure failed, error = ', errorCode, ', message = ', errorMessage);
+    END IF;
+
+END$$
+DELIMITER ;
+
+-- Call the stored procedure
+CALL updateCareProvider;
+DROP PROCEDURE updateCareProvider;
