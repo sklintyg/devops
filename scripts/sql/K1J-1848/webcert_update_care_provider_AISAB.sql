c5b434b22a78068e35fed4888503366a476961f8
cigerhed <christoffer.sigerhed@soprasteria.com>
2026-03-25 17:31:13 +0100

    Add temporary table to webcert.

--- scripts/sql/K1J-1848/webcert_update_care_provider_AISAB.sql
+++ scripts/sql/K1J-1848/webcert_update_care_provider_AISAB.sql
@@ -10,54 +10,61 @@ BEGIN
     DECLARE errorCode CHAR(5) DEFAULT '00000';
     DECLARE errorMessage TEXT;
 
-    DECLARE originalCareProviderId VARCHAR(50);
-
     -- Declare handler
     DECLARE CONTINUE HANDLER FOR SQLEXCEPTION
-    BEGIN
-        GET DIAGNOSTICS CONDITION 1
-            errorCode = RETURNED_SQLSTATE, errorMessage = MESSAGE_TEXT;
-    END;
+BEGIN
+GET DIAGNOSTICS CONDITION 1
+    errorCode = RETURNED_SQLSTATE, errorMessage = MESSAGE_TEXT;
+END;
 
-    SET updatedCareProviderId = 'SE2321000016-A9KJ';
-    SET updatedCareProviderName = 'Region Stockholm';
-    SET originalCareProviderId = 'SE2321000016-A1A6DT'
+    SET updatedCareProviderId = 'SE2321000016-39KJ';
+    SET updatedCareProviderName = 'Stockholms läns landsting';
 
     -- Start transaction
-    START TRANSACTION;
+START TRANSACTION;
 
-    -- Inactivate safe-updates as we are updating rows based on other columns than primary keys
-    SET SQL_SAFE_UPDATES = 0;
+-- Inactivate safe-updates as we are updating rows based on other columns than primary keys
+SET SQL_SAFE_UPDATES = 0;
 
-    -- Update FRAGASVAR table
-    UPDATE FRAGASVAR
-    SET VARDGIVAR_ID = updatedCareProviderId, VARDGIVARNAMN = updatedCareProviderName
-    WHERE VARDGIVAR_ID = originalCareProviderId;
+    DROP TEMPORARY TABLE IF EXISTS originalCareProviderIds;
+    CREATE TEMPORARY TABLE originalCareProviderIds(id VARCHAR(50)
+       NOT NULL COLLATE utf8mb3_general_ci -- Set the collate to same as the tables otherwise a table-scan will be made.
+   );
 
-    -- Update HANDELSE table
-    UPDATE HANDELSE
-    SET VARDGIVAR_ID = updatedCareProviderId
-    WHERE VARDGIVAR_ID = originalCareProviderId;
+    -- Insert original care provider IDs into the table variable
+INSERT INTO originalCareProviderIds
+VALUES ('SE2321000016-16DT');
 
-    -- Update INTEGRERADE_VARDENHETER table
-    UPDATE INTEGRERADE_VARDENHETER
-    SET VARDGIVAR_ID = updatedCareProviderId, VARDGIVAR_NAMN = updatedCareProviderName
-    WHERE VARDGIVAR_ID = originalCareProviderId;
+-- Update FRAGASVAR table
+UPDATE FRAGASVAR
+SET VARDGIVAR_ID = updatedCareProviderId, VARDGIVARNAMN = updatedCareProviderName
+WHERE VARDGIVAR_ID IN (SELECT Id FROM originalCareProviderIds);
 
-    -- Update INTYG table
-    UPDATE INTYG
-    SET VARDGIVAR_ID = updatedCareProviderId, VARDGIVAR_NAMN = updatedCareProviderName
-    WHERE VARDGIVAR_ID = originalCareProviderId;
+-- Update HANDELSE table
+UPDATE HANDELSE
+SET VARDGIVAR_ID = updatedCareProviderId
+WHERE VARDGIVAR_ID IN (SELECT Id FROM originalCareProviderIds);
 
+-- Update INTEGRERADE_VARDENHETER table
+UPDATE INTEGRERADE_VARDENHETER
+SET VARDGIVAR_ID = updatedCareProviderId, VARDGIVAR_NAMN = updatedCareProviderName
+WHERE VARDGIVAR_ID IN (SELECT Id FROM originalCareProviderIds);
+
+-- Update INTYG table
+UPDATE INTYG
+SET VARDGIVAR_ID = updatedCareProviderId, VARDGIVAR_NAMN = updatedCareProviderName
+WHERE VARDGIVAR_ID IN (SELECT Id FROM originalCareProviderIds);
+
+DROP TEMPORARY TABLE IF EXISTS originalCareProviderIds;
 
     IF errorCode = '00000' THEN
         COMMIT;
-        SELECT 'Updated care provider successfully.';
-    ELSE
+SELECT 'Updated care provider successfully.';
+ELSE
         ROLLBACK;
-        SELECT 'Transaction rolled back due to sql exception. No changes were introduced.';
-        SELECT CONCAT('Stored procedure failed, error = ', errorCode, ', message = ', errorMessage);
-    END IF;
+SELECT 'Transaction rolled back due to sql exception. No changes were introduced.';
+SELECT CONCAT('Stored procedure failed, error = ', errorCode, ', message = ', errorMessage);
+END IF;
 
     -- Activate safe-updates again
     SET SQL_SAFE_UPDATES = 1;
